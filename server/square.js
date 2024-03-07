1b01db39316bd473bb8151217c9cc4f6cae5d937
EAnthonycarranza <129874785+EAnthonycarranza@users.noreply.github.com>
2024-03-07 15:44:49 -0600

    Add files via upload

--- server/square.js
+++ server/square.js
@@ -1,6 +1,10 @@
 const { ApiError, Client, Environment } = require('square');
 
-const { isProduction, SQUARE_ACCESS_TOKEN } = require('./config');
+// Determine if the application is running in production or sandbox mode
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Directly assign your Square access token here
+const SQUARE_ACCESS_TOKEN = 'EAAAl4uAAKHNLsNGP1RxWM17XSyIyEGo6fC7GQUg2ixmixPyZ04yzgyRaO8bluVo';
 
 const client = new Client({
   environment: isProduction ? Environment.Production : Environment.Sandbox,
